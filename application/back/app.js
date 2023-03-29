2e691668a81a916c5431633ac6d198a43c87588a
ItsAlanWorld <alaneelee@gmail.com>
2023-03-29 11:45:46 +0900

    initial state

---
+++ application/back/app.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const cors = require('cors');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const passport = require('passport');
+const dotenv = require('dotenv');
+const hpp = require('hpp');
+const helmet = require('helmet');
+
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+const userRouter = require('./routes/user');
+const materialRouter = require('./routes/material');
+const photoRouter = require('./routes/photo');
+const db = require('./models');
+const passportConfig = require('./passport');
+
+dotenv.config();
+
+const app = express();
+
+db.sequelize.sync({ force: false })
+  .then(() => {
+    console.log('db connection success');
+  })
+  .catch(console.error);
+
+passportConfig();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
+  app.use(hpp());
+  app.use(helmet());
+} else {
+  app.use(morgan('dev'));
+}
+
+app.use(cors({
+  origin: ['https://forestgreening.shop'],
+  credentials: true,
+}));
+
+app.options('/', cors());
+
+// app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.SECRET));
+app.use(session({
+  saveUninitialized: false,
+  resave: false,
+  secret: process.env.SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: true,
+    domain: process.env.NODE_ENV === 'production' && '.forestgreening.shop',
+  },
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/user', userRouter);
+app.use('/material', materialRouter);
+app.use('/photo', photoRouter);
+
+app.listen(3065, () => {
+  console.log('서버 실행 중');
+});
---
+++ application/back/models/photo.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+  const Photo = sequelize.define('Photo', {
+    imageUrl: {
+      type: DataTypes.TEXT,
+    },
+    latitude: {
+      type: DataTypes.FLOAT(20),
+    },
+    longitude: {
+      type: DataTypes.FLOAT(20),
+    },
+    hash: {
+      type: DataTypes.STRING(100),
+    },
+    creator: {
+      type: DataTypes.STRING(20),
+    },
+  }, {
+    charset: 'utf8',
+    colate: 'utf8_general_ci',
+    tableName: 'photos',
+  });
+  Photo.associate = (db) => {
+    db.Photo.belongsTo(db.User);
+  };
+
+  return Photo;
+};
